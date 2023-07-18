457bd2b4717bb2b97ec4795190dcae3a043fc450
jmt-xm3 <128064150+jmt-xm3@users.noreply.github.com>
2023-07-18 11:24:41 +0100

    we got colours to change
    removed random psd files
    got createDazzle() to actually create the car folder and create a temp png recolouring the first section and a final decals.png

--- livery_classes.py
+++ livery_classes.py
@@ -2,9 +2,10 @@ from changeColour import change_colour
 
 import os
 
-currentDirectory = os.getcwd()
-print(currentDirectory)
+def hexToTuple(hex):
+  return tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
 
+currentDirectory = os.getcwd()
 accCarModels = [{"carModelType": 31 , "dazzleTemplate":"EVO2DAZZLE.png"}]
 
 class ACCLivery:
@@ -20,14 +21,88 @@ class ACCLivery:
         self.rimTapeMaterialId = 0
         self.raceNumber = 000
         self.DazzleTopColour = (208, 42, 64)
-        self.DazzleBottomColour = (74, 73, 135) # im not letting people fuck with the decals layer json cba same with decals (yet?)
+        self.DazzleBottomColour = (74, 73, 135) # im not letting people fuck with the decals layer json cba same with sponsors (yet?)
  # Getter methods
+    def setFolderName(self, folderName):
+        self._folderName = folderName
+
+    def setInGameName(self, inGameName):
+        self._inGameName = inGameName
+
+    def setCarModelType(self, carModelType):
+        self._carModelType = carModelType
+
+    def setBaseColour(self, baseColour):
+        self._baseColour = baseColour
+
+    def setBaseMaterialId(self, baseMaterialId):
+        self._baseMaterialId = baseMaterialId
+
+    def setRimColour(self, rimColour):
+        self._rimColour = rimColour
+
+    def setRimMaterialId(self, rimMaterialId):
+        self._rimMaterialId = rimMaterialId
+
+    def setRimTapeColour(self, rimTapeColour):
+        self._rimTapeColour = rimTapeColour
+
+    def setRimTapeMaterialId(self, rimTapeMaterialId):
+        self._rimTapeMaterialId = rimTapeMaterialId
+
+    def setRaceNumber(self, raceNumber):
+        self.raceNumber = raceNumber
+
+    def setDazzleTopColour(self, DazzleTopColour):
+        self.DazzleTopColour = DazzleTopColour
+
+    def setDazzleBottomColour(self, DazzleBottomColour):
+        self.DazzleBottomColour = DazzleBottomColour
+
+     # Setter methods
+    def setFolderName(self, folderName):
+        self.folderName = folderName
+
+    def setInGameName(self, inGameName):
+        self.inGameName = inGameName
+
+    def setCarModelType(self, carModelType):
+        self.carModelType = carModelType
+
+    def setBaseColour(self, baseColour):
+        self.baseColour = baseColour
+
+    def setBaseMaterialId(self, baseMaterialId):
+        self.baseMaterialId = baseMaterialId
+
+    def setRimColour(self, rimColour):
+        self.rimColour = rimColour
+
+    def setRimMaterialId(self, rimMaterialId):
+        self.rimMaterialId = rimMaterialId
+
+    def setRimTapeColour(self, rimTapeColour):
+        self.rimTapeColour = rimTapeColour
+
+    def setRimTapeMaterialId(self, rimTapeMaterialId):
+        self.rimTapeMaterialId = rimTapeMaterialId
+
+    def setRaceNumber(self, raceNumber):
+        self.raceNumber = raceNumber
+
+    def setDazzleTopColour(self, DazzleTopColour):
+        self.DazzleTopColour = DazzleTopColour
+
+    def setDazzleBottomColour(self, DazzleBottomColour):
+        self.DazzleBottomColour = DazzleBottomColour
+
+    # Getter methods
     def getFolderName(self):
         return self.folderName
-    
+
     def getInGameName(self):
         return self.inGameName
-    
+
     def getCarModelType(self):
         return self.carModelType
 
@@ -53,59 +128,30 @@ class ACCLivery:
         return self.raceNumber
 
     def getDazzleTopColour(self):
-        return self.dazzleTopColour
+        return self.DazzleTopColour
 
     def getDazzleBottomColour(self):
-        return self.dazzleBottomColour
-
-    # Setter methods
-    def setCarModelType(self, value):
-        self.carModelType = value
-
-    def setBaseColour(self, value):
-        self.baseColour = value
-    
-    def setFolderName(self, value):
-        self.folderName = value
-
-    def setInGameName(self, value):
-        self.inGameName = value
-
-    def setBaseMaterialId(self, value):
-        self.baseMaterialId = value
-
-    def setRimColour(self, value):
-        self.rimColour = value
-
-    def setRimMaterialId(self, value):
-        self.rimMaterialId = value
-
-    def setRimTapeColour(self, value):
-        self.rimTapeColour = value
-
-    def setRimTapeMaterialId(self, value):
-        self.rimTapeMaterialId = value
-
-    def setRaceNumber(self, value):
-        self.raceNumber = value
+        return self.DazzleBottomColour
 
     # Other methods
     def createDazzle(self):
         for car in accCarModels:
             if car['carModelType'] == self.carModelType:
                 dazzleTemplate = car['dazzleTemplate']
-        dazzlePath = currentDirectory + "/acc/"+ dazzleTemplate
-        carPath = "/home/jonan0/Documents/GitHub/xm3Bot/"+"Liveries/"+self.folderName
+        dazzlePath = currentDirectory + "/acc/"
+        dazzlePath = os.path.join(dazzlePath, dazzleTemplate)
+        carPath = os.path.join(currentDirectory, self.folderName)
         try:
-            os.mkdir(carPath)
+            os.mkdir(self.folderName)
         except FileExistsError:
             pass
         first = change_colour(dazzlePath, (0,0,0), self.DazzleTopColour)
         first.save(carPath+"/temp.png")
-        final = change_colour(carPath+"/first.png", (255,255,255), self.Dazzle)
+        final = change_colour(carPath+"/temp.png", (255,255,255), self.DazzleBottomColour)
         final.save(carPath+"/decals.png")
 
 car1 = ACCLivery()
+car1.setDazzleTopColour((0,0,0))
 car1.createDazzle()       
 
 
