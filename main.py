12da6402f8e5b12735d319aed2d21584c80b7a10
jmt-xm3 <128064150+jmt-xm3@users.noreply.github.com>
2023-07-20 23:35:47 +0100

    idk

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import random
-from discord.ext import commands
 import discord
 import os
 import shutil
-
+import datetime
 from liveryClasses import accCarModels, ACCLivery
+from discord.ext import commands
 
 
+startTime = datetime.datetime.now()
+materials = [{'key':'Glossy','value': 0}]
 cars = [{'key': 'AMR V12 Vantage GT3', 'value': 12}, {'key': 'AMR V8 Vantage', 'value': 20}, {'key': 'Audi R8 LMS Evo', 'value': 19}, {'key': 'Audi R8 LMS GT3 Evo 2', 'value': 31}, {'key': 'Audi R8 LMS', 'value': 3}, {'key': 'BMW M4 GT3', 'value': 30}, {'key': 'BMW M6 GT3', 'value': 7}, {'key': 'Bentley Continental GT3 2015', 'value': 11}, {'key': 'Bentley Continental GT3 2018', 'value': 8}, {'key': 'Emil Frey Jaguar G3', 'value': 14}, {'key': 'Ferrari 296 GT3', 'value': 32}, {'key': 'Ferrari 488 GT3 Evo', 'value': 24}, {'key': 'Ferrari 488 GT3', 'value': 2}, {'key': 'Honda NSX GT3 Evo', 'value': 21}, {'key': 'Honda NSX GT3', 'value': 17}, {
     'key': 'Lamborghini Huracán GT3 Evo', 'value': 16}, {'key': 'Lamborghini Huracán GT3 EVO2', 'value': 33}, {'key': 'Lamborghini Huracán GT3', 'value': 4}, {'key': 'Lexus RC F GT3', 'value': 15}, {'key': 'McLaren 650S GT3', 'value': 5}, {'key': 'McLaren 720S GT3', 'value': 22}, {'key': 'McLaren 720S GT3 EVO', 'value': 35}, {'key': 'Mercedes-AMG GT3', 'value': 1}, {'key': 'Mercedes-AMG GT3 Evo', 'value': 25}, {'key': 'Nissan GT-R Nismo GT3 2015', 'value': 10}, {'key': 'Nissan GT-R Nismo GT3 2018', 'value': 6}, {'key': 'Porsche 991 GT3 R', 'value': 0}, {'key': 'Porsche 991 II GT3 R', 'value': 23}, {'key': 'Porsche 992 GT3 R', 'value': 34}, {'key': 'Reiter Engineering R-EX GT3', 'value': 13}]
 available = []
@@ -48,29 +50,15 @@ async def on_ready():
     print('------')
 
 
-@bot.command()
-async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left + right)
+
 
 
 @bot.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
-
-
-@bot.command(description='For when you wanna settle the score some other way')
-async def choose(ctx, *choices: str):
-    """Chooses between multiple choices."""
-    await ctx.send(random.choice(choices))
+async def uptime(ctx):
+    currentTime = datetime.datetime.now()
+    difference = (currentTime - startTime).total_seconds() / 60
+    await ctx.send(f'This bot has been running for {difference} minutes since {str(startTime)}')
+    
 
 
 @bot.command()
@@ -96,7 +84,6 @@ async def joined(ctx, member: discord.Member):
 class carDropdown(discord.ui.Select):
     def __init__(self):
         options = []
-        self.carModel = 0
         for car in cars:
             options.append(discord.SelectOption(label=car["key"], description='Select this for a '+car['key']+' livery.', emoji='🏎️'))
 
@@ -113,7 +100,7 @@ class carDropdown(discord.ui.Select):
         await interaction.response.send_message(f'You selected the {self.values[0]}')
         for car in cars:
             if car['key'] == self.values[0]:
-                self.carModel = car['value']
+                newCarLivery.setCarModelType(car['value'])
                 break
 
 
@@ -122,22 +109,52 @@ class carDropdownView(discord.ui.View):
         super().__init__()
         dropdown = carDropdown()
         self.add_item(dropdown)
-        self.CMT = dropdown.carModel
-        print(self.CMT)
+
+class satinDropdown(discord.ui.Select):
+    def __init__(self):
+        options = []
+        materials = []
+        for car in cars:
+            options.append(discord.SelectOption(label=car["key"], description='Select this for a '+car['key']+' base.', emoji='🏎️'))
+
+        # The placeholder is what will be shown when no option is chosen
+        # The min and max values indicate we can only pick one of the three options
+        # The options parameter defines the dropdown options. We defined this above
+        super().__init__(placeholder='Choose a car', min_values=1, max_values=1, options=options)
+
+    async def callback(self, interaction: discord.Interaction):
+        # Use the interaction object to send a response message containing
+        # the user's favourite colour or choice. The self object refers to the
+        # Select object, and the values attribute gets a list of the user's
+        # selected options. We only want the first one.
+        await interaction.response.send_message(f'You selected the {self.values[0]}')
+        for car in cars:
+            if car['key'] == self.values[0]:
+                newCarLivery.setCarModelType(car['value'])
+                break
+
+
+class satinDropdownView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        dropdown = carDropdown()
+        self.add_item(dropdown)
+        
+    
         
         
 
 @bot.command()
 async def newLivery(ctx):
+    global newCarLivery
     """Sends a message with our dropdown containing colours"""
     newCarLivery = ACCLivery()
     # Create the view containing our dropdown
-    view = carDropdownView()
+    carView = carDropdownView()
 
     # Sending a message containing our view
-    await ctx.send('Pick the car for your new livery:', view=view)
-    newCarLivery.setCarModelType(view.CMT)
-    print('car model type is ', newCarLivery.getCarModelType())
+    await ctx.send('Pick the car for your new livery:', view=carView)
+    
 
-
+    
 bot.run('MTEyOTE5MDk3MTc5NjYzNTY0OA.GRSyl9.HIHgEWFNQ-VTt1MiyRYZDF41iutxWUAussL-So')
